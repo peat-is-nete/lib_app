2b168be480dcdaf44a1b269c7d0d36483349c8fa
kiran <kiranrakesh@gmail.com>
2021-04-10 11:18:18 -0500

    Add User and UserRole models

--- src/main/java/com/lib/library/model/User.java
+++ src/main/java/com/lib/library/model/User.java
@@ -1,4 +1,12 @@
 package com.lib.library.model;
 
 public class User {
+    private final int CHECKOUT_LIMIT = 5;
+
+    private Long id;
+    private String first_name;
+    private String last_name;
+    private String password;
+    private int role_id;
+
 }
---
+++ src/main/java/com/lib/library/model/UserRole.java
@@ -0,0 +1,7 @@
+package com.lib.library.model;
+
+public class UserRole {
+    private Long id;
+    private int type;
+    private String description;
+}
