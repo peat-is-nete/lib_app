8180d29569670e362a7b47fc4c2d11378ee8e4d7
kiran <kiranrakesh@gmail.com>
2021-04-12 14:59:04 -0500

    Add bookRepisitory and categoryRepository

--- src/main/java/com/lib/library/repository/BookRepository.java
+++ src/main/java/com/lib/library/repository/BookRepository.java
@@ -1,2 +1,16 @@
-package com.lib.library.repository;public class BookRepository {
+package com.lib.library.repository;
+
+import com.lib.library.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+    Book findByTitle(String title);
+
+    List<Book> findByCategoryId(Long categoryId);
 }
+
+
