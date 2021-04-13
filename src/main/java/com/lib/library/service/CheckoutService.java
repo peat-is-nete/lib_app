b956af91a47d6a8a5a1a1e2760635d83701a3c5f
Pete Micsunescu <peat.is.nete@gmail.com>
2021-04-13 11:53:16 -0500

    Autowired 'UserRepository' and 'BookRepository' interfaces in 'CheckoutService'.

--- src/main/java/com/lib/library/service/CheckoutService.java
+++ src/main/java/com/lib/library/service/CheckoutService.java
@@ -1,9 +1,26 @@
 package com.lib.library.service;
 
+import com.lib.library.repository.BookRepository;
+import com.lib.library.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CheckoutService {
+
+    UserRepository userRepository;
+    BookRepository bookRepository;
+
+    @Autowired
+    public void setUserRepository(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @Autowired
+    public void setBookRepository(BookRepository bookRepository){
+        this.bookRepository = bookRepository;
+    }
+
 
     
 
