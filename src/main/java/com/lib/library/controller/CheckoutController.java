da0210f1a827f637dc951511b0f3dd410502c232
Pete Micsunescu <peat.is.nete@gmail.com>
2021-04-13 12:07:18 -0500

    Autowired 'CheckoutService' interface in 'CheckoutController'.

--- src/main/java/com/lib/library/controller/CheckoutController.java
+++ src/main/java/com/lib/library/controller/CheckoutController.java
@@ -1,12 +1,27 @@
 package com.lib.library.controller;
 
 
+import com.lib.library.service.CheckoutService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/checkout")
 public class CheckoutController {
+
+    private CheckoutService checkoutService;
+
+    @Autowired
+    public void setCheckoutService(CheckoutService checkoutService) {
+        this.checkoutService = checkoutService;
+    }
+
+
+
+
+
+
 
 
 
