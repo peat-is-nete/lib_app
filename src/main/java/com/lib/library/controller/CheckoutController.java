aa50cf579657dc873800bb9683f67e7647e5c6e4
Pete Micsunescu <peat.is.nete@gmail.com>
2021-04-13 11:45:57 -0500

    Created 'Checkout Controller' in 'controller' package.

---
+++ src/main/java/com/lib/library/controller/CheckoutController.java
@@ -0,0 +1,13 @@
+package com.lib.library.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/checkout")
+public class CheckoutController {
+
+
+    
+}
