32738c1230c732751216fe1e03f416fb873e5b50
kiran <kiranrakesh@gmail.com>
2021-04-14 09:27:46 -0500

    create CheckoutLimitReachException

---
+++ src/main/java/com/lib/library/exception/CheckoutLimitReachException.java
@@ -0,0 +1,11 @@
+package com.lib.library.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
+public class CheckoutLimitReachException extends RuntimeException {
+    public CheckoutLimitReachException(String message) {
+        super (message);
+    }
+}
