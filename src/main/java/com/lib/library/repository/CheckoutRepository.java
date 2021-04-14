94c609690590deb29066f9bb09698f9af0996887
kiran <kiranrakesh@gmail.com>
2021-04-14 09:29:20 -0500

    Create CheckoutRepository

---
+++ src/main/java/com/lib/library/repository/CheckoutRepository.java
@@ -0,0 +1,15 @@
+package com.lib.library.repository;
+
+import com.lib.library.model.Checkout;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CheckoutRepository extends JpaRepository<Checkout, Long> {
+
+    List<Checkout> findByUserId(Long userId);
+    Checkout getCheckoutByUserIdAndBookId(Long userID, Long bookId);
+    List<Checkout> getAllCheckoutsByUserId(Long checkoutId);
+}
